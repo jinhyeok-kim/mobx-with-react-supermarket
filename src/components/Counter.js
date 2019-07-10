281593f515925f0975f1d7f4f1f6d817dcc735fd
KimJinhyeok <persona5173@gmail.com>
2019-07-10 10:03:53 +0900

    Practice Mobx-with-react

---
+++ src/components/Counter.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { observer, inject } from 'mobx-react';
+
+// @inject('counter')
+// @observer
+// class Counter extends Component {
+//   render() {
+//       const { counter } = this.props;
+//     return (
+//       <div>
+//         <h1>{counter.number}</h1>
+//         <button onClick={counter.increase}>+1</button>
+//         <button onClick={counter.decrease}>-1</button>
+//       </div>
+//     );
+//   }
+// }
+
+// **** 함수형태로 파라미터를 전달해주면 특정 값만 받아올 수 있음.
+@inject(stores => ({
+    number: stores.counter.number,
+    increase: stores.counter.increase,
+    decrease: stores.counter.decrease,
+  }))
+  @observer
+  class Counter extends Component {
+    render() {
+      const { number, increase, decrease } = this.props;
+      return (
+        <div>
+          <h1>{number}</h1>
+          <button onClick={increase}>+1</button>
+          <button onClick={decrease}>-1</button>
+        </div>
+      );
+    }
+  }
+  
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'mobx-react';
+import CounterStore from './stores/counter.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const counter = new CounterStore(); // 스토어 인스턴스를 만들고
+
+ReactDOM.render(
+    <Provider counter={counter}>
+      {/* Provider 에 props 로 넣어줍니다. */}
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
+  
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
