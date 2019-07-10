2edab4bb9eb01440ba5880fc8adb85bcf80563cf
KimJinhyeok <persona5173@gmail.com>
2019-07-10 16:45:13 +0900

    Refactoring & Optimizing

--- src/components/BasketItemList.js
+++ src/components/BasketItemList.js
@@ -2,12 +2,10 @@ import React from 'react';
 import BasketItem from './BasketItem';
 import { inject, observer } from 'mobx-react';
 
-const BasketItemList = ({ items, total, onTake }) => {
+const BasketItemList = ({ items, onTake }) => {
   const itemList = items.map(item => (
     <BasketItem
-      name={item.name}
-      price={item.price}
-      count={item.count}
+      item={item}
       key={item.name}
       onTake={onTake}
     />
@@ -15,16 +13,11 @@ const BasketItemList = ({ items, total, onTake }) => {
   return (
     <div>
       {itemList}
-      <hr />
-      <p>
-        <b>총합: </b> { total }원
-      </p>
     </div>
   );
 };
 
 export default inject(({ market }) => ({
   items: market.selectedItems,
-  total: market.total,
   onTake: market.take,
 }))(observer(BasketItemList));
