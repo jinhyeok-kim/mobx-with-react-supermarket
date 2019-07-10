9c2149c3f21779ee316c5707ab285230cabf3f9d
KimJinhyeok <persona5173@gmail.com>
2019-07-10 10:44:16 +0900

    Add market store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'mobx-react';
-import CounterStore from './stores/counter.js';
+import CounterStore from './stores/counter';
+import MarketStore from './stores/market'
 
 const counter = new CounterStore(); // 스토어 인스턴스를 만들고
+const market = new MarketStore();
 
 ReactDOM.render(
-    <Provider counter={counter}>
+    <Provider counter={counter} market={market}>
       {/* Provider 에 props 로 넣어줍니다. */}
       <App />
     </Provider>,
